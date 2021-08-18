d971cea1b3dec9530cc8074f9b01bcd2d4dbb1dd
Bunny <mypython007@gmail.com>
2021-08-18 15:15:31 +0530

    under construction update

--- components/Construction.js
+++ components/Construction.js
@@ -1,23 +1,46 @@
 import Image from 'next/image';
+import {CodeIcon, EmojiHappyIcon} from '@heroicons/react/outline';
 
 function Construction() {
     return (
-        <div className="h-screen w-full p-5 md:p-11 bg-red-100 flex">
-            <div className="flex flex-col">
-                {/** logo */}
-
+        <div className="h-screen p-5 md:mx-11 md:pt-11 flex">
+            <div className="flex flex-col w-full md:w-1/2">
+            <div className="pt-11">
+                <Image className="animate-pulse"
+                    alt="Image Alt"
+                    src="/logo.png"
+                    height="75"
+                    width="75"
+                />
+            </div>
+            <h1 className="text-blue-600 tracking-widest font-bold">Elite <span className="text-red-800">Cricket</span></h1>
                 {/** H1 */}
-                <p className="text-4xl md:text-6xl uppercase font-bold text-white py-6">we're launching soon</p>
+                <p className=" text-3xl md:text-6xl uppercase font-bold text-white py-6">we're launching soon </p>
                 {/** Content */}
                 <p className="py-4">We're creating something exciting in the house and about to launch. 
                     Thank you so much for your interest.
                 </p>
                 {/** Social */}
                 <p className="text-gray-100 text-sm font-bold">Stay Connected</p>
+                <div className="pt-3 flex">
+                    <div className="mx-1">
+                        <Image className="cursor-pointer " src="/facebook.svg" height="25" width="25"/>
+                    </div>
+                    <div className="mx-1">
+                        <Image className="cursor-pointer " src="/instagram.svg" height="25" width="25"/>
+                    </div>
+                    <div className="mx-1">
+                        <Image className="cursor-pointer " src="/youtube.svg" height="25" width="25"/>
+                    </div>
+
+                </div>
                 {/** Copyrights */}
                 <p className="text-gray-100 text-sm font-bold pt-11">&copy; Copyright 2021. All Rights Resverved</p>
+
             </div>
-            <div></div>
+            <div className=" md:w-1/2 justify-center items-center hidden md:flex">
+                        <CodeIcon height="250" className="text-black text-center" />
+            </div>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Construction from '../components/Construction';
 
 export default function Home() {
   return (
-    <div className="h-screen w-full bg-indigo-100">
+    <div className="h-screen w-full bg-red-400">
       <Head>
         <title>Elite Cricket</title>
         <link rel="icon" href="/favicon.ico" />
